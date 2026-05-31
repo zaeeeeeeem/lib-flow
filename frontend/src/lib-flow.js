3ced61051bde02d9b69c65fee111fd905b517a48
zaeeeeeeem <dyour253@gmail.com>
2026-05-31 04:27:18 -0700

    added 2 files

---
+++ frontend/src/lib-flow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <div style={{ textAlign: 'center', marginTop: '50px', color: '#fff', fontFamily: 'sans-serif' }}>
+      <h1>LibraFlow E-Library</h1>
+      <p>Frontend is up and running!</p>
+    </div>
+  </React.StrictMode>
+);
